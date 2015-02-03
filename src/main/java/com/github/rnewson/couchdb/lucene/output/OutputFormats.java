736edb7d49aaeeb67b7f0dbdf95e408113653900
obdulia-losantos <obdulia.losantos@gmail.com>
2015-02-03 13:11:56 +0100

    Include transform method in enum

--- src/main/java/com/github/rnewson/couchdb/lucene/output/OutputFormats.java
+++ src/main/java/com/github/rnewson/couchdb/lucene/output/OutputFormats.java
@@ -1,22 +1,56 @@
 package com.github.rnewson.couchdb.lucene.output;
 
+import org.json.CDL;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 /**
  * Available output formats
  */
 public enum OutputFormats {
 
-    JSON("json"),
-    XML("xml"),
-    CSV("csv");
+    JSON("json", "application/json"),
+    XML("xml", "application/xml"),
+    CSV("csv", "text/plain");
 
-    private String value;
+    private String formatType;
+    private String contentType;
 
-    private OutputFormats(String value) {
-        this.value = value;
+    private OutputFormats(String type, String contentType) {
+        this.formatType = type;
+        this.contentType = contentType;
     }
 
     @Override
     public String toString() {
-        return this.value;
+        return this.formatType;
+    }
+
+    public String getContentType() {
+        return this.contentType;
+    }
+
+    /**
+     * Transforms the given array of documents in the corresponding format
+     * mapped by the list of keys
+     *
+     * @param docs the array of documents
+     * @param keys the list of properties to be mapped
+     * @return the string expression of the formatted documents
+     * @throws JSONException
+     */
+    public String transformDocs(final JSONArray docs,
+                                final String[] keys)
+            throws JSONException {
+
+        switch (this) {
+            case XML:
+                return org.json.XML.toString(docs, "doc");
+            case CSV:
+                return CDL.toString(JSONUtils.flat(docs, keys));
+            case JSON:
+            default:
+                return docs.toString();
+        }
     }
 }
---
+++ src/test/java/com/github/rnewson/couchdb/lucene/output/OutputFormatsTest.java
@@ -0,0 +1,77 @@
+package com.github.rnewson.couchdb.lucene.output;
+
+import org.json.JSONArray;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test OutputFormat enum
+ */
+public class OutputFormatsTest {
+
+    @Test
+    public void testJSONFormat() throws Exception {
+        JSONArray docs = getFixture();
+        OutputFormats format = OutputFormats.JSON;
+
+        String result = format.transformDocs(docs, null);
+
+        // nothing change
+        assertThat(result, is(docs.toString()));
+    }
+
+    @Test
+    public void testJSONFormatWithNames() throws Exception {
+        JSONArray docs = getFixture();
+        OutputFormats format = OutputFormats.JSON;
+
+        String result = format.transformDocs(docs, null);
+
+        // nothing change
+        assertThat(result, is(docs.toString()));
+    }
+
+    @Test
+    public void testXMLFormat() throws Exception {
+        JSONArray docs = getFixture();
+        OutputFormats format = OutputFormats.XML;
+        String result = format.transformDocs(docs, null);
+
+        String expected = "<doc><baz>1</baz><bar><foo>1</foo></bar></doc>" +
+                "<doc><baz>2</baz><bar><foo>2</foo></bar></doc>" +
+                "<doc><baz>3</baz><bar><foo>3</foo></bar></doc>" +
+                "<doc><baz>4</baz><bar><foo>4</foo></bar></doc>";
+
+        // nothing change
+        assertThat(result, is(expected));
+    }
+
+
+    @Test
+    public void testCSVFormat() throws Exception {
+        JSONArray docs = getFixture();
+        OutputFormats format = OutputFormats.CSV;
+        String result = format.transformDocs(docs, null);
+
+        String expected = "bar.foo,baz\n" +
+                "1,1\n" +
+                "2,2\n" +
+                "3,3\n" +
+                "4,4\n";
+
+        // nothing change
+        assertThat(result, is(expected));
+    }
+
+    private JSONArray getFixture() throws Exception {
+        String input = "[" +
+                "{ \"bar\" : { \"foo\" : 1 }, \"baz\" : 1 }," +
+                "{ \"bar\" : { \"foo\" : 2 }, \"baz\" : 2 }," +
+                "{ \"bar\" : { \"foo\" : 3 }, \"baz\" : 3 }," +
+                "{ \"bar\" : { \"foo\" : 4 }, \"baz\" : 4 }" +
+                "]";
+        return new JSONArray(input);
+    }
+}
